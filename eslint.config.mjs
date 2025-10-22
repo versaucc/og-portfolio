47affa7a11e92519d28c42f6c102f312156fb114
Oliver <oliisbali@gmail.com>
2025-10-22 07:17:52 -0700

    relax eslint no-sync-scripts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,9 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
+    rules: {
+      "@next/next/no-sync-scripts": "off"
+    }, 
     ignores: [
       "node_modules/**",
       ".next/**",
